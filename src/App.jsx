cd489abe1b3adadabb36c83f9c6059b978bca80d
jsonJae <jasongian28@gmail.com>
2025-12-13 19:55:26 +0800

    feat: added the about me image and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experiences from './components/Experiences';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
-    <div className = "overflow-hidden">
+    <div className = "overflow-hidden max-w-screen">
       <Navbar />
       <Hero />
+      <About />
       <Projects />
       <Skills />
       <Experiences />
---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import aboutMePic from '../assets/AboutMePic.png';
+
+export default function About(){
+    const aboutMe1 = 
+        "A junior student at UPLB and a designer and developer who bridges creativity and code. I craft intuitive digital experiences through thoughtful design and bring them to life with clean, efficient development."
+    const aboutMe2 = 
+        "With expertise spanning graphic design, UI/UX, and full-stack development, I approach every project with both an aesthetic eye and technical precision. Whether I'm creating visual identities, sketching wireframes, or debugging code, my goal is to create solutions that are not only beautiful but functional and user-centered.";
+    const aboutMe3 = 
+        "I believe great digital products exist at the intersection of form and function – where elegant design meets robust engineering. Let's build something remarkable together.";
+
+    return (
+        <div className="flex flex-row w-full">
+
+            <div className="flex flex-1 justify-center">
+                <div className="flex flex-col max-w-110 justify-center">
+                    <p className="text-[64px] text-[#0824A9] font-hartwell text-center mb-2 mt-10">
+                        Hey, I'm Jason
+                    </p>
+
+                    <div>
+                        <p className="text-[12px] text-black font-ambulatoria text-justify mt-2">
+                            {aboutMe1}
+                        </p>
+                        <p className="text-[12px] text-black font-ambulatoria text-justify mt-2">
+                            {aboutMe2}
+                        </p>
+                        <p className="text-[12px] text-black font-ambulatoria text-justify mt-2 mb-6">
+                            {aboutMe3}
+                        </p>
+                    </div>
+
+                    <button className='bg-blue-500 cursor-pointer'>Contact Me</button>
+                </div>
+            </div>
+
+            <img src={aboutMePic} className="shrink-0" alt="About Me"/>
+        </div>
+    );
+}
