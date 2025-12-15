b9f509c083aa695e91048a4ee023cc774a234dd8
jsonJae <jasongian28@gmail.com>
2025-12-15 13:58:28 +0800

    feat: added the icons and added an onlcick functions to them to open links in new tabs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faSquareLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 import aboutMePic from '../assets/AboutMePic.png';
+
+
+
 
 export default function About(){
     const aboutMe1 = 
@@ -9,7 +17,7 @@ export default function About(){
         "I believe great digital products exist at the intersection of form and function – where elegant design meets robust engineering. Let's build something remarkable together.";
 
     return (
-        <div className="flex flex-row w-full">
+        <div className="flex flex-row w-full min-h-screen">
 
             <div className="flex flex-1 justify-center">
                 <div className="flex flex-col max-w-110 justify-center">
@@ -17,19 +25,23 @@ export default function About(){
                         Hey, I'm Jason
                     </p>
 
-                    <div>
+                    <div className='space-y-2'>
                         <p className="text-[12px] text-black font-ambulatoria text-justify mt-2">
                             {aboutMe1}
                         </p>
                         <p className="text-[12px] text-black font-ambulatoria text-justify mt-2">
                             {aboutMe2}
                         </p>
-                        <p className="text-[12px] text-black font-ambulatoria text-justify mt-2 mb-6">
+                        <p className="text-[12px] text-black font-ambulatoria text-justify mt-2 mb-4">
                             {aboutMe3}
                         </p>
                     </div>
 
-                    <button className='bg-blue-500 cursor-pointer'>Contact Me</button>
+                    <div className='flex gap-2'>
+                        <FontAwesomeIcon icon={faGithub} size="xl" style={{color: "#0824a9",}} className='cursor-pointer' onClick={() => window.open('https://github.com/jsonJae')}/>
+                        <FontAwesomeIcon icon={faSquareLinkedin} size="xl" style={{color: "#0824a9",}} className='cursor-pointer' onClick={() => window.open('https://www.linkedin.com/in/jason-gian-duran-4754a7355?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')} />
+                        <FontAwesomeIcon icon={faEnvelope} size="xl" style={{color: "#0824a9",}} onClick={() => window.open('mailto:jasongian28@gmail.com')} className='cursor-pointer' />
+                    </div>
                 </div>
             </div>
 
